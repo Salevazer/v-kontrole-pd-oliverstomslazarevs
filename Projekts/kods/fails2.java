74e70c8bcfd0a71548a7900ac5e8a3cec6be735c
Salevazer <oliverstomslazarevs@gmail.com>
2025-12-15 16:55:47 +0200

    pievienots jauns fails par git, izdzēsta mapīte src, pievienota jauna mape kods un tajā salikti 3 programmu faili

---
+++ Projekts/kods/fails2.java
@@ -0,0 +1,8 @@
+package org.example;
+public class fails2 {
+
+    public static void main(String[] args) {
+        String text = "Kad skolēni sabojā git projektu: Nu ko… laiks jaunam klonam.";
+        String[] strings = text.split("");
+    }
+}
---
+++ Projekts/kods/svarigi.java
@@ -0,0 +1,7 @@
+package org.example;
+public class svarigi {
+
+    public static void main(String[] args) {
+        System.out.println("Dzīve nekādā veidā nav iedomājama bez git");
+    }
+}
